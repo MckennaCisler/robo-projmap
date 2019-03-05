150f3011ce25de2930bfa31970a11633cd7bbfff
Henry Stone <henryvstone@gmail.com>
2019-03-04 20:54:24 -0500

    added basic kinect reading

---
+++ kinect.py
@@ -0,0 +1,21 @@
+from freenect2 import Device, FrameType
+import numpy as np
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+print('Opening Kinect via USB')
+device = Device()
+
+def get_color_frame():
+    with device.running():
+        for type_, frame1 in device:
+            if type_ is FrameType.Color:
+                break
+        return frame1
+
+def get_depth_frame():
+    with device.running():
+        for type_, frame1 in device:
+            if type_ is FrameType.Depth:
+                break
+        return frame1
