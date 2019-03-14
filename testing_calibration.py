bb791f0bfa5d9ef2cca4b7a9b982cb5ff850e5cf
Mckenna Cisler <mckennacisler@gmail.com>
2019-03-13 21:15:03 -0400

    Attempt to fix old kinect library

--- testing_calibration.py
+++ testing_calibration.py
@@ -6,6 +6,8 @@ from project import Fullscreen_Window
 from time import sleep
 import cv2
 import matplotlib.pyplot as plt
+from freenect2 import Device, FrameType
+import gc
 
 w = Fullscreen_Window()
 w.clear()
@@ -19,29 +21,46 @@ projector = Camera(p_intr, p_dist, x_res=1366, y_res=768).getCameraWithExtr(p_ex
 y_coords = np.ones([1920]) * 1080 / 2
 x_coords = np.arange(1920)
 
-k = Kinect()
-k.start()
+# k = Kinect()
+# k.start()
 
-for i in range(100):
-    _, d = k.get_current_rgbd_frame() # (None, np.ones((1080, 1920)))
+device = Device()
 
-    d_coord = d[540, :]
+j = 0
+with device.running():
+    for i in range(4):
+        color = None
+        for type_, frame1 in device:
+                j += 1
+                if j > 100:
+                    break
 
-    world_space = camera.get_world_space(y=y_coords, x=x_coords, depth=d_coord / 30.0)
-    projector_space = np.round(projector.get_camera_space(world_space)).astype(np.int32)
+                if type_ is FrameType.Color:
+                    color = frame1
+                elif type_ is FrameType.Depth and color is not None:
+                    _, _, d = device.registration.apply(color, frame1, with_big_depth=True)
+                    d = d.to_array()[1:-1,::-1]
+                    # _, d = k.get_current_rgbd_frame() # (None, np.ones((1080, 1920)))
 
-    projector_space_valid = np.logical_and(
-        np.logical_and(projector_space[..., 0] >= 0, projector_space[..., 0] < 1368),
-        np.logical_and(projector_space[..., 1] >= 0, projector_space[..., 1] < 768))
+                    d_coord = d[540, :]
 
-    projector_space = projector_space[projector_space_valid, :]
+                    world_space = camera.get_world_space(y=y_coords, x=x_coords, depth=d_coord / 30.0)
+                    projector_space = np.round(projector.get_camera_space(world_space)).astype(np.int32)
 
-    w.clear()
-    w.draw_points(projector_space[..., 0].flatten(), projector_space[..., 1].flatten(), fill='white', s=5, outline='white')
-    sleep(0.05)
+                    projector_space_valid = np.logical_and(
+                        np.logical_and(projector_space[..., 0] >= 0, projector_space[..., 0] < 1368),
+                        np.logical_and(projector_space[..., 1] >= 0, projector_space[..., 1] < 768))
+
+                    projector_space = projector_space[projector_space_valid, :]
+
+                    gc.collect()
+
+                    # w.clear()
+                    # w.draw_points(projector_space[..., 0].flatten(), projector_space[..., 1].flatten(), fill='white', s=5, outline='white')
+                    # sleep(0.05)
 
 # rgb,_  = k.get_current_rgbd_frame()
 # cv2.imwrite('testing.png', rgb)
 
-k.stop()
+# k.stop()
 print(projector_space)
