0d71e8c6d14f47afa1afa5e4e6d433994e82a446
Henry Stone <henry_stone1@brown.edu>
2019-04-03 20:40:15 -0400

    more c gl

--- project_gl.py
+++ project_gl.py
@@ -3,9 +3,9 @@ import numpy as np
 
 class Projector():
     def __init__(self, calibration_matrix, *, y_res, x_res):
-        self.calibration_matrix = self.calibration_matrix
-        self.inds = np.indices([y_res, x_res]).T
-        project.start()
+        self.calibration_matrix = calibration_matrix
+        self.inds = np.indices([y_res, x_res], dtype=np.float32).transpose([1, 2, 0])
+        #project.start()
 
     def draw_frame(self, rgb, depth):
         depth = np.expand_dims(depth, -1)
@@ -14,7 +14,16 @@ class Projector():
             depth,
             rgb
         ], -1)
-        project.draw_frame(coords)
+        #project.draw_frame(coords)
 
     def __del__(self):
-        project.stop()
+        #project.stop()
+        pass
+
+
+if __name__ == '__main__':
+    p = Projector(np.zeros([4, 4]), y_res=1080, x_res=1920)
+    rgb = np.zeros([1080, 1920, 3], dtype=np.uint8)
+    depth = np.zeros([1080, 1920], dtype=np.float32)
+    for i in range(100):
+        p.draw_frame(rgb, depth)
